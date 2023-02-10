24b7dccc44d094b9432f4cfa4511c4dd5c22224f
Nathaly <carolinamarb31@gmail.com>
2023-02-09 21:18:07 -0500

    Agregando documentos

---
+++ scriipt.js
@@ -0,0 +1,14 @@
+function encriptar (){
+        var texto = document.querySelector("input-texto").value;
+        var texto Cifrado = texto.replace [/e/gi, "enter"] replace [/i/gi, "imes"] replace [/a/gi, "ai"] replace [/o/gi, "ober"] replace [/u/gi, "ufat"];
+        document.querySelector(".text-input").value;
+        document.querySelector("input-texto").value;
+
+}
+
+
+function encriptar (){
+        var texto = document.querySelector("input-texto").value;
+        var texto Cifrado = texto.replace [/e/gi, "enter"] replace [/i/gi, "imes"] replace [/a/gi, "ai"] replace [/o/gi, "ober"] replace [/u/gi, "ufat"];
+        document.querySelector(".text-input").value;
+        document.querySelector("input-texto").value;
